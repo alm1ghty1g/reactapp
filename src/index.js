17c6ea71b25b89c6bb5449f58e315f8252b21b48
Kirill Sazonov <allm1ghty1g@gmail.com>
2021-05-15 05:09:23 -0400

    commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// noprotect
+import React from "react";
+import ReactDOM from "react-dom";
+import "bulma/css/bulma.min.css";
+import "./index.css";
+import App from "./App";
+import Amplify from "aws-amplify";
+import configuration from "./configuration";
+import * as serviceWorker from "./serviceWorker";
+import config from "./aws-exports";
+
+Amplify.configure(config);
+
+Amplify.configure({
+  Auth: {
+    mandatorySignedIn: true,
+    region: configuration.cognito.REGION,
+    userPoolId: configuration.cognito.USER_POOL_ID,
+    userPoolWebClientId: configuration.cognito.APP_CLIENT_ID,
+  },
+});
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
